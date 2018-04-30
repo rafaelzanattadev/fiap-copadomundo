1b5f51711a42642bccd8bd2dcb8684f075178b6f
Rafael Barradas Zanatta <36791629+rafaelzanattadev@users.noreply.github.com>
2018-04-30 19:16:28 -0300

    fiap12net
    fiap12net

---
+++ fiap-copadomundo/fp_18_web_aula_1_core/Models/Time.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace fp_web_aula_1_core.Models
+{
+
+    public class Time
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [EmailAddress]
+        public string EnderecoDeEmail { get; set; }
+        public string Bandeira { get; set; }
+        public bool Publicado { get; set; }
+    }
+}
